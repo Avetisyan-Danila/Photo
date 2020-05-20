0dd2b100ae9261f6879ea191e6a34ecb51af9911
d_a-git-projects <aboriginnn@yandex.ru>
2020-05-20 22:36:24 +0300

    add adaptability for all devices and burger menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+// Плавная прокрутка к якорю
 $("body").on('click', '[href*="#"]', function (e) {
-    var fixed_offset = 88;
+    if (document.documentElement.clientWidth > 767) {
+        var fixed_offset = 88;
+    } else {
+        var fixed_offset = 58;
+    }
     $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
     e.preventDefault();
+});
+
+// Меню и бургер для телефонов
+$(document).ready(function () {
+    $(".header__burger").click(function (e) {
+        $(".header__burger, .header__menu").toggleClass("active");
+        $("body").toggleClass('lock');
+    });
+    $(".header__link").click(function (e) { 
+        $(".header__burger").removeClass("active");
+        $(".header__menu").removeClass("active");
+        $("body").removeClass("lock");
+    });
 });
